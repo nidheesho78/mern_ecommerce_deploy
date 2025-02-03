20f68482076938aa13e584a65cb92c992fb5498c
nidheesho78 <nidheesho979@gmail.com>
2025-02-03 07:04:41 +0530

    server side changes

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -130,8 +130,8 @@ const logoutUser = (req, res) => {
 // };
 
 const authMiddleware = async (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
   if (!token)
     return res.status(401).json({
       success: false,
